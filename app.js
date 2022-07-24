a262b70e0c4e1a7b4dbcb9660add6fa809cf2665
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2022-07-24 16:01:11 +0100

    a football leagues app

---
+++ app.js
@@ -0,0 +1,48 @@
+// fetch('https://serpapi.com/search.json?q=premier+league&location=austin%2C+texas%2C+united+states&api_key=2a67890f0f70ab3850f19b8716a9d2e48f4311ab394f3c2ceadee5c3f179d0ba').then((response) => {
+
+//     if(!response.ok)
+//     throw new Error(`Error Status Code: ${response.status}`);
+
+//     return response.json().then((data) => {
+//         console.log(data);
+//     })
+// })
+// .catch((err) => {
+//     console.log('Catch Error');
+//     console.log(err);
+// });
+
+// let request = {
+//     method: 'GET',
+//     url: 'https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/team',
+//     params: {name: 'Liverpool'},
+//     headers: {
+//       'x-rapidapi-host': 'heisenbug-premier-league-live-scores-v1.p.rapidapi.com',
+//       'x-rapidapi-key': '5ba045cd1emshb214db5d56ac35bp1c2043jsn71361b99d184'
+//     }
+//   };
+  
+//   fetch(request).then((response) => {
+//       return response.json();
+//   }).then((data) => {
+//       console.log(data)
+//   })
+//   .catch(function (err) {
+//       console.log(err);
+//   });
+
+//This is the Github for the api below: https://github.com/azharimm/football-standings-api
+
+// fetch('https://api-football-standings.azharimm.site/leagues').then((response) => {
+
+//     if(!response.ok)
+//     throw new Error(`Error Status Code: ${response.status}`);
+
+//     return response.json().then((data) => {
+//         console.log(data);
+//     })
+// })
+// .catch((err) => {
+//     console.log('Catch Error');
+//     console.log(err);
+// });
